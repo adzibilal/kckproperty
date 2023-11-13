8f74bbac9eee55293791c8414e70b0a3e7d3d709
adzibilal <adzibilal02@gmail.com>
2023-11-13 12:21:34 +0700

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Karya Cipta Property',
-    description: 'Ciptakan masa depanmu bersama Karya Cipta Property'
+    description: 'Ciptakan masa depanmu bersama Karya Cipta Property',
 }
 
 export default function RootLayout({
--- app/tanjungkadedeuhresidence/layout.tsx
+++ app/tanjungkadedeuhresidence/layout.tsx
@@ -1,4 +1,25 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
+
+export const metadata: Metadata = {
+    title: 'Tanjung Kadedeuh Residence by KCKPROPERTY',
+    description: 'Hunian Komersil Strategis, Cicilan Mulai 1,5 Juta/Bulan',
+    icons: '/images/tkr/favicon.png',
+    openGraph: {
+        type: 'website',
+        locale: 'id_ID',
+        url: 'https://www.kckproperty.com/tanjungkadedeuhresidence',
+        siteName: 'Tanjung Kadedeuh Residence by KCKPROPERTY',
+        images: [
+            {
+                url: '/images/tkr/hero-img.jpeg',
+                width: 800,
+                height: 600,
+                alt: 'Tanjung Kadedeuh Residence by KCKPROPERTY'
+            }
+        ]
+    }
+}
 
 const TKRLayout = ({ children }: { children: React.ReactNode }) => {
     return <div className=''>{children}</div>
